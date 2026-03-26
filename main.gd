47894ac7111e959ab3d30beee38d4fae28d37088
cembatdal <cembatdal@gmail.com>
2026-03-27 00:32:57 +0300

    Add main scene and preload map/card/hud
    Add Main scene (main.tscn) and script (main.gd) that preloads and instantiates map_scene, card_scene, and hud_scene. New scene files (map_scene.tscn, card_scene.tscn, hud_scene.tscn) were added; main.gd adds them as children and hides the card instance by default. Update project.godot to set the new Main scene as the run/main_scene.

---
+++ main.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var is_card_active: bool = false
+
+var map_scene_template = preload("res://map_scene.tscn")
+var card_scene_template = preload("res://card_scene.tscn")
+var hud_scene_template = preload("res://hud_scene.tscn")
+
+func _ready() -> void:
+	var map_instance = map_scene_template.instantiate()
+	var card_instance = card_scene_template.instantiate()
+	var hud_instance = hud_scene_template.instantiate()
+
+	add_child(map_instance)
+	add_child(card_instance)
+	add_child(hud_instance)
+	
+	card_instance.hide()
